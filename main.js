b92021504a9a1b48677e4f7a506e87d03b5d3736
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-05 15:26:04 +0200

    add script train ticket

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log("Hi there")
+
+/* Il programma dovrà chiedere all'utente
+ -il numero di chilometri che vuole percorrere  
+ -l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio secondo queste regole:
+ -il prezzo del biglietto è definito in base ai km (0.21 € al km)
+ -va applicato uno sconto del 20% per i minorenni
+ -va applicato uno sconto del 40% per gli over 65.
+ -L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+  
+*/
+
+/*tools
+ -let/const
+ -console.log
+ -if/else-if/else
+*/
+
+// utente fornisce numero km
+
+const kilometres = prompt("inserisci km da percorrere")
+console.log(kilometres)
+
+// utente fornisce età passeggeri
+
+const age = prompt("inserisci età")
+console.log(age)
+
+// calcolo costo biglietto
+
+let price = kilometres * 0.21
+console.log(price)
+
+// sconto under 18 e sconto over 65
+
+if (age < 18) {
+    price = price - (price * 20 / 100)
+    console.log(price)
+} else if (age > 65) {
+    price = price - (price * 40 / 100)
+    console.log(price)
+} else {
+    console.log(price)
+}
+
+console.log(price.toFixed(2))
